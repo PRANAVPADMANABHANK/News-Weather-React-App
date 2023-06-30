a8af631ebbcf016f34524daecdc6d67dd5c0e6be
pranav k <pranavkkeloth@gmail.com>
2023-06-30 15:55:35 +0530

    fetched the api using redux-thunk middleware and added a loading animation before fetching the data using beatloader

--- src/Components/FetchNews/fetchNews.js
+++ src/Components/FetchNews/fetchNews.js
@@ -1,49 +1,58 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import { fetchNews } from "../../Redux/Slice/news";
 import "../FetchNews/fetchNews.css";
+import { useDispatch, useSelector } from "react-redux";
+import { css } from "@emotion/react";
+import { BeatLoader } from "react-spinners";
 
 const FetchNews = () => {
-  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   useEffect(() => {
-    getNews();
-  }, []);
+    dispatch(fetchNews());
+  }, [dispatch]);
 
-  const getNews = () => {
-    axios
-      .get(
-        "https://newsapi.org/v2/everything?q=apple&from=2023-06-28&to=2023-06-28&sortBy=popularity&apiKey=7d3bb61252064a1b82860e8f5f6fa1b9"
-      )
-      .then((response) => {
-        console.log(response);
-        setData(response.data.articles);
-      });
-  };
+  // Beatloader component from react-spinners library to show animation
+  if (state.news.isLoading) {
+    const override = css`
+      display: block;
+      margin: 0 auto;
+      margin-top: 20px;
+    `;
+
+    return (
+      <div className="loading-container">
+        <BeatLoader color="#ffffff" loading={true} css={override} size={15} />
+      </div>
+    );
+  }
 
   return (
     <>
       <div className="container">
         <div className="row">
-          {data.map((value, key) => (
-            <div className="col-4 mb-4" key={key}>
-              <div className="card news-card h-100">
-                {value.urlToImage && (
-                  <img
-                    src={value.urlToImage}
-                    className="card-img-top"
-                    alt={value.title}
-                  />
-                )}
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{value.title}</h5>
-                  <p className="card-text">{value.description}</p>
-                  <a href={value.url} className="btn btn-primary mt-auto">
-                    Read More
-                  </a>
+          {state.news.data?.articles &&
+            state.news.data.articles.map((article, index) => (
+              <div className="col-4 mb-4" key={index}>
+                <div className="card news-card h-100">
+                  {article.urlToImage && (
+                    <img
+                      src={article.urlToImage}
+                      className="card-img-top"
+                      alt={article.title}
+                    />
+                  )}
+                  <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">{article.title}</h5>
+                    <p className="card-text">{article.description}</p>
+                    <a href={article.url} className="btn btn-primary mt-auto">
+                      Read More
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
--- src/Redux/Slice/news.js
+++ src/Redux/Slice/news.js
@@ -1,9 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-
+// Action
+export const fetchNews = createAsyncThunk('fetchNews', async ()=>{
+    const response =await fetch('https://newsapi.org/v2/everything?q=apple&from=2023-06-28&to=2023-06-28&sortBy=popularity&apiKey=7d3bb61252064a1b82860e8f5f6fa1b9');
+    return response.json();
+})
 
 const newsSlice = createSlice({
   name: "news",
+  initialState:{
+    isLoading: false,
+    data: null
+  },
+  extraReducers: (builder)=>{
+    builder.addCase(fetchNews.pending,(state,action)=>{
+        state.isLoading = true
+    })
+    builder.addCase(fetchNews.fulfilled, (state, action)=>{
+        state.isLoading = false;
+        state.data = action.payload;
+    })
+    builder.addCase(fetchNews.rejected, (state,action)=>{
+        console.log("Error", action.payload)
+        state.isError = true;
+    })
+  }
 });
 
 export default newsSlice.reducer;

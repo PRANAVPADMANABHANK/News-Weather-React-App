e7ea4e13a282e0953e74c721da587338df9bef27
pranav k <pranavkkeloth@gmail.com>
2023-07-01 22:24:14 +0530

    proper comment added

--- src/api/api.js
+++ src/api/api.js
@@ -7,8 +7,8 @@ export const geoApiOptions = {
   },
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; //url endpoint for fetching the cities 
 
 //Weather api from openweathermap.org
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; //url endpoint for fetching current weather and day forecasting
 export const WEATHER_API_KEY = "9bd22c3a3899a34848d9f46e4e5dd6fe";

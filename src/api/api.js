746f7372eb3ee0a095644966dd631d89c4b5b006
pranav k <pranavkkeloth@gmail.com>
2023-08-10 23:55:30 +0530

    feat: Implement api bug fix

--- src/api/api.js
+++ src/api/api.js
@@ -14,4 +14,4 @@ export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; //url 
 export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 //News api key from newsapi.org
-export const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+export const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;   
